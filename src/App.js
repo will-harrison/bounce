21255e8cd895f509d6990f25f2c1d34f09a0b3c8
Will Harrison <willharrison@gmail.com>
2020-02-03 15:28:40 -0800

    WIP, bouncing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMeasure } from "react";
+import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+import _ from "lodash";
+import "./App.css";
+
+const dotCount = 5;
+const moveSpeed = 300;
+const bounceHeight = 150;
+const interp = i => r => {
+  console.log(bounceHeight * Math.sin(r + (i % 2)));
+
+  return `translate3d(0, ${bounceHeight * Math.sin(r + (i % 2))}px, ${2 *
+    Math.sin(r + (i % 2))}em)`;
+};
 
 function App() {
+  const { move } = useSpring({
+    to: async next => {
+      while (1) await next({ move: moveSpeed });
+    },
+    from: { move: 0 }, // starting point
+    config: { duration: 50000 },
+    reset: true
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBody>
+      {_.map(_.range(dotCount), (dot, index) => (
+        <AnimatedDot
+          style={{
+            transform: move.interpolate(interp(index))
+          }}
+          key={index}
+        />
+      ))}
+    </AppBody>
   );
 }
+
+const AppBody = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const Dot = styled.div`
+  background-color: #336699;
+  border-radius: 50%;
+  height: 100px;
+  width: 100px;
+`;
+
+const AnimatedDot = animated(Dot);
 
 export default App;
